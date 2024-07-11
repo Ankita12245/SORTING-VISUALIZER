43cea965dbf2f3d78f156ba54733e33d31748da5
Ankita12245 <makhijaankita892@gmail.com>
2024-07-11 21:22:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,599 @@
+// swap function util for sorting algorithms takes input of 2 DOM elements with .style.height feature
+function swap(el1, el2) {    
+    let temp = el1.style.height;    //third variable for swapping
+    el1.style.height = el2.style.height;
+    el2.style.height = temp;
+}
+
+// Disables sorting buttons used in conjunction with enable, so that we can disable during sorting and enable buttons after it
+function disableSortingBtn(){
+    document.querySelector(".bubbleSort").disabled = true;
+    document.querySelector(".insertionSort").disabled = true;
+    document.querySelector(".mergeSort").disabled = true;
+    document.querySelector(".quickSort").disabled = true;
+    document.querySelector(".selectionSort").disabled = true;
+    document.querySelector(".heapSort").disabled = true;
+}
+
+// Enables sorting buttons used in conjunction with disable
+function enableSortingBtn(){
+    document.querySelector(".bubbleSort").disabled = false;
+    document.querySelector(".insertionSort").disabled = false;
+    document.querySelector(".mergeSort").disabled = false;
+    document.querySelector(".quickSort").disabled = false;
+    document.querySelector(".selectionSort").disabled = false;
+    document.querySelector(".heapSort").disabled = false;
+}
+
+// Disables size slider used in conjunction with enable, so that we can disable during sorting and enable buttons after it
+function disableSizeSlider(){
+    document.querySelector("#size_input").disabled = true;
+}
+
+function disableSpeedSlider(){
+    document.querySelector("#speed_input").disabled = true;
+}
+
+function enableSpeedSlider(){
+    document.querySelector("#speed_input").disabled = false;
+}
+
+// Enables size slider used in conjunction with disable
+function enableSizeSlider(){
+    document.querySelector("#size_input").disabled = false;
+}
+
+// Disables newArray buttons used in conjunction with enable, so that we can disable during sorting and enable buttons after it
+function disableNewArrayBtn(){
+    document.querySelector(".new").disabled = true;
+}
+
+function enableNewArrayBtn(){
+    document.querySelector(".new").disabled = false;
+}
+
+function enableStopSortingBtn(){
+    document.querySelector(".stop").disabled = false;
+}
+
+function disableStopSortingBtn(){
+    document.querySelector(".stop").disabled = true;
+}
+
+// Used in async function so that we can so animations of sorting, takes input time in ms (1000 = 1s)
+function delayTime(milisec) { 
+    return new Promise(resolve => { 
+        setTimeout(() => { resolve('') }, milisec); 
+    }) 
+}
+
+// Selecting size slider from DOM
+let arraySize = document.querySelector('#size_input');
+
+// Event listener to update the bars on the UI
+arraySize.addEventListener('input', function(){
+    console.log(arraySize.value, typeof(arraySize.value));
+    createNewArray(parseInt(arraySize.value));
+});
+
+// Default input for waitforme function (260ms)
+let delay = 260;
+
+// Selecting speed slider from DOM
+let delayElement = document.querySelector('#speed_input');
+
+// Event listener to update delay time 
+delayElement.addEventListener('input', function(){
+    console.log(delayElement.value, typeof(delayElement.value));
+    delay = 320 - parseInt(delayElement.value);
+});
+
+// Creating barArray to store randomly generated numbers
+let barArray = [];
+
+// Call to display bars right when you visit the site
+createNewArray();
+
+// To create new barArray input size of barArray
+function createNewArray(noOfBars = 60) {
+    // calling helper function to delete old bars from dom
+    deleteChild();
+
+    // creating an barArray of random numbers 
+    barArray = [];
+    for (let i = 0; i < noOfBars; i++) {
+        barArray.push(Math.floor(Math.random() * 251));
+    }
+    console.log(barArray);
+
+    const bars = document.querySelector("#sorting");
+
+    // create multiple element div using loop and adding class 'bar col'
+    for (let i = 0; i < noOfBars; i++) {
+        const bar = document.createElement("div");
+        bar.style.height = `${barArray[i]*2}px`;
+        bar.classList.add('bar');
+        bar.classList.add('flex-item');
+        bar.classList.add(`barNo${i}`);
+        bars.appendChild(bar);
+    }
+}
+
+// Helper function to delete all the previous bars so that new can be added
+function deleteChild() {
+    const bar = document.querySelector("#sorting");
+    bar.innerHTML = '';
+}
+
+// Selecting newarray button from DOM and adding eventlistener
+const newArrayButton = document.querySelector(".new");
+newArrayButton.addEventListener("click", function(){
+    hasPressedStop = false;
+    enableSpeedSlider();
+    console.log("From newArray " + arraySize.value);
+    console.log("From newArray " + delay);
+    enableSortingBtn();
+    enableSizeSlider();
+    createNewArray(arraySize.value);
+});
+
+const stopSortingButton = document.querySelector(".stop");
+stopSortingButton.addEventListener("click", function(){
+    disableSortingBtn();
+    disableSizeSlider();
+    hasPressedStop = true;
+})
+let hasPressedStop = new Boolean(false);
+
+async function bubble() {
+    const ele = document.querySelectorAll(".bar");
+    for(let i = 0; i < ele.length-1; i++){
+        for(let j = 0; j < ele.length-i-1; j++){
+            if(hasPressedStop == true){
+                return;
+            }
+            ele[j].style.background = 'cyan';
+            ele[j+1].style.background = 'cyan';
+            if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)){
+                await delayTime(delay);
+                swap(ele[j], ele[j+1]);
+            }
+            ele[j].style.background = '#e43f5a';
+            ele[j+1].style.background = '#e43f5a';
+        }
+        ele[ele.length-1-i].style.background = 'green';
+    }
+    ele[0].style.background = 'green';
+}
+
+const bubSortbtn = document.querySelector(".bubbleSort");
+bubSortbtn.addEventListener('click', async function(){
+    hasPressedStop = false;
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    enableStopSortingBtn();
+    await bubble();
+    if(hasPressedStop==true){
+        disableSpeedSlider();
+    } else {
+        enableSortingBtn();
+        enableSizeSlider();
+    }
+    enableNewArrayBtn();
+    disableStopSortingBtn();
+});
+async function merge(ele, low, mid, high){
+    console.log('In merge()');
+    const n1 = mid - low + 1;
+    const n2 = high - mid;
+    let left = new Array(n1);
+    let right = new Array(n2);
+
+    for(let i = 0; i < n1; i++){
+        if(hasPressedStop==true){
+            return;
+        }
+        await delayTime(delay);
+        ele[low + i].style.background = 'orange';
+        left[i] = ele[low + i].style.height;
+    }
+    for(let i = 0; i < n2; i++){
+        if(hasPressedStop==true){
+            return;
+        }
+        await delayTime(delay);
+        ele[mid + 1 + i].style.background = 'cyan';
+        right[i] = ele[mid + 1 + i].style.height;
+    }
+    await delayTime(delay);
+    let i = 0, j = 0, k = low;
+    while(i < n1 && j < n2){
+        if(hasPressedStop==true){
+            return;
+        }
+        await delayTime(delay);
+        
+        if(parseInt(left[i]) <= parseInt(right[j])){
+            if((n1 + n2) === ele.length){
+                ele[k].style.background = 'green';
+            }
+            else{
+                ele[k].style.background = 'lightgreen';
+            }
+            
+            ele[k].style.height = left[i];
+            i++;
+            k++;
+        }
+        else{
+            if((n1 + n2) === ele.length){
+                ele[k].style.background = 'green';
+            }
+            else{
+                ele[k].style.background = 'lightgreen';
+            } 
+            ele[k].style.height = right[j];
+            j++;
+            k++;
+        }
+    }
+    while(i < n1){
+        if(hasPressedStop==true){
+            return;
+        }
+        await delayTime(delay);
+        if((n1 + n2) === ele.length){
+            ele[k].style.background = 'green';
+        }
+        else{
+            ele[k].style.background = 'lightgreen';
+        }
+        ele[k].style.height = left[i];
+        i++;
+        k++;
+    }
+    while(j < n2){
+        if(hasPressedStop==true){
+            return;
+        }
+        await delayTime(delay);
+        if((n1 + n2) === ele.length){
+            ele[k].style.background = 'green';
+        }
+        else{
+            ele[k].style.background = 'lightgreen';
+        }
+        ele[k].style.height = right[j];
+        j++;
+        k++;
+    }
+}
+
+async function mergeSort(ele, l, r){
+    if(l >= r){
+        //sorting complete
+        return;
+    }
+    const m = l + Math.floor((r - l) / 2);
+    await mergeSort(ele, l, m);
+    await mergeSort(ele, m + 1, r);
+    await merge(ele, l, m, r);
+}
+
+const mergeSortbtn = document.querySelector(".mergeSort");
+mergeSortbtn.addEventListener('click', async function(){
+    let ele = document.querySelectorAll('.bar');
+    let l = 0;
+    let r = parseInt(ele.length) - 1;
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    enableStopSortingBtn();
+    await mergeSort(ele, l, r);
+    if(hasPressedStop==true){
+        disableSpeedSlider();
+    } else {
+        enableSortingBtn();
+        enableSizeSlider();
+    }
+    enableNewArrayBtn();
+    disableStopSortingBtn();
+});
+async function partitionLomuto(ele, l, r){
+    let i = l - 1;
+    ele[r].style.background = 'cyan'; //pivot
+    for(let j = l; j <= r - 1; j++){
+        if(hasPressedStop == true){
+            return;
+        }
+        ele[j].style.background = 'yellow'; //current element
+        await delayTime(delay);
+        if(hasPressedStop == true){
+            return;
+        }
+        if(parseInt(ele[j].style.height) < parseInt(ele[r].style.height)){
+            i++;
+            swap(ele[i], ele[j]);
+            // color 
+            ele[i].style.background = 'orange';
+            if(i != j) ele[j].style.background = 'orange';
+            // pauseChamp
+            await delayTime(delay);
+        }
+        else{
+            // color if not less than pivot
+            ele[j].style.background = 'pink';
+        }
+    }
+    i++; 
+    if(hasPressedStop == true){
+        return;
+    }
+    await delayTime(delay);
+    if(hasPressedStop == true){
+        return;
+    }
+    swap(ele[i], ele[r]);
+    // color
+    ele[r].style.background = 'pink';
+    ele[i].style.background = 'green';
+
+    if(hasPressedStop == true){
+        return;
+    }
+    await delayTime(delay);
+    if(hasPressedStop == true){
+        return;
+    }
+    
+    // color
+    for(let k = 0; k < ele.length; k++){
+        if(ele[k].style.background != 'green')
+            ele[k].style.background = '#e43f5a';
+    }
+
+    return i;
+}
+
+async function quickSort(ele, l, r){
+    if(l < r){
+        let pivot_index = await partitionLomuto(ele, l, r);
+        await quickSort(ele, l, pivot_index - 1);
+        await quickSort(ele, pivot_index + 1, r);
+    }
+    else{
+        if(l >= 0 && r >= 0 && l <ele.length && r <ele.length){
+            ele[r].style.background = 'green';
+            ele[l].style.background = 'green';
+        }
+    }
+}
+
+
+const quickSortbtn = document.querySelector(".quickSort");
+quickSortbtn.addEventListener('click', async function(){
+    let ele = document.querySelectorAll('.bar');
+    let l = 0;
+    let r = ele.length - 1;
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    enableStopSortingBtn();
+    await quickSort(ele, l, r);
+    if(hasPressedStop==true){
+        disableSpeedSlider();
+    } else {
+        enableSortingBtn();
+        enableSizeSlider();
+    }
+    enableNewArrayBtn();
+    disableStopSortingBtn();
+});
+async function selection(){
+    const ele = document.querySelectorAll(".bar");
+    for(let i = 0; i < ele.length; i++){
+        if(hasPressedStop==true){
+            return;
+        }
+        let min_index = i;
+        // Change color of the bar being compared
+        ele[i].style.background = 'lightgreen';
+        for(let j = i+1; j < ele.length; j++){
+            if(hasPressedStop==true){
+                return;
+            }
+            // Change color of current bar
+            ele[j].style.background = 'cyan';
+
+            await delayTime(delay);
+            if(hasPressedStop==true){
+                return;
+            }
+            if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height)){
+                if(min_index !== i){
+                    // new min_index is found so change prev min_index color back to normal
+                    ele[min_index].style.background = '#e43f5a';
+                }
+                min_index = j;
+            } 
+            else{
+                // if the currnent comparision is more than min_index change is back to normal
+                ele[j].style.background = '#e43f5a';
+            }   
+        }
+        await delayTime(delay);
+        if(hasPressedStop==true){
+            return;
+        }
+        swap(ele[min_index], ele[i]);
+        // change the min element index back to normal as it is swapped 
+        ele[min_index].style.background = '#e43f5a';
+        // change the sorted elements color to green
+        ele[i].style.background = 'green';
+    }
+}
+
+const selectionSortbtn = document.querySelector(".selectionSort");
+selectionSortbtn.addEventListener('click', async function(){
+    hasPressedStop = false;
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    enableStopSortingBtn();
+    await selection();
+    if(hasPressedStop==true){
+        disableSpeedSlider();
+    } else {
+        enableSortingBtn();
+        enableSizeSlider();
+    }
+    enableNewArrayBtn();
+    disableStopSortingBtn();
+});
+//TO:DO fix the colors in heapify
+
+async function heapSort(arr, n) {
+    // Build heap (rearrange array)
+    for (var i = n / 2 - 1; i >= 0; i--) {
+        if(hasPressedStop == true){
+            return;
+        }
+        await heapify(arr, n, i);
+    }
+
+    // One by one extract an element from heap
+    for (var i = n - 1; i > 0; i--) {
+        if(hasPressedStop == true){
+            return;
+        }
+        // Move current root to end
+        var temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        arr[0].style.background = 'cyan';
+        arr[i].style.background = 'green';
+        swap(arr[0], arr[i]);
+        await delayTime(delay);
+
+        // call max heapify on the reduced heap
+        await heapify(arr, i, 0);
+    }
+}
+
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
+async function heapify(arr, n, i) {
+    if(hasPressedStop == true){
+        return;
+    }
+    var largest = i; // Initialize largest as root
+    var l = 2 * i + 1; // left = 2*i + 1
+    var r = 2 * i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    
+    if (l < n && parseInt(arr[l].style.height) > parseInt(arr[largest].style.height)) {
+        // arr[l].style.background = 'lightblue'; 
+        // arr[largest].style.background = 'cyan';
+        largest = l;
+        swap(arr[largest], arr[l]);
+        // arr[l].style.background = '#e43f5a';
+    }
+    
+
+    // If right child is larger than largest so far
+    if (r < n && parseInt(arr[r].style.height) > parseInt(arr[largest].style.height)) { 
+        // arr[r].style.background = 'lightgreen';
+        // arr[largest].style.background = 'cyan'; 
+        largest = r;
+        swap(arr[largest], arr[r]);
+        // arr[l].style.background = '#e43f5a'; 
+    }
+
+    // If largest is not root
+    if (largest != i) {
+        var temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+const heapSortbtn = document.querySelector(".heapSort");
+heapSortbtn.addEventListener("click", async function () {
+    let arr = document.querySelectorAll('.bar');
+    let n = arr.length;
+
+    hasPressedStop = false;
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    enableStopSortingBtn();
+    await heapSort(arr, n);
+    arr[0].style.background = 'green';
+    if (hasPressedStop == true) {
+        disableSpeedSlider();
+    } else {
+        enableSortingBtn();
+        enableSizeSlider();
+    }
+    enableNewArrayBtn();
+    disableStopSortingBtn();
+});
+async function insertion(){
+    const ele = document.querySelectorAll(".bar");
+    ele[0].style.background = 'green';
+    for(let i = 1; i < ele.length; i++){
+        if(hasPressedStop==true){
+            return;
+        }
+        let j = i - 1;
+        let key = ele[i].style.height;
+        ele[i].style.background = 'blue';
+
+        await delayTime(delay);
+        if(hasPressedStop==true){
+            return;
+        }
+
+        while(j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))){
+            if(hasPressedStop==true){
+                return;
+            }
+            ele[j].style.background = 'blue';
+            ele[j + 1].style.height = ele[j].style.height;
+            j--;
+
+            await delayTime(delay);
+            if(hasPressedStop==true){
+                return;
+            }
+            for(let k = i; k >= 0; k--){
+                ele[k].style.background = 'green';
+            }
+        }
+        ele[j + 1].style.height = key;
+        ele[i].style.background = 'green';
+    }
+}
+
+const inSortbtn = document.querySelector(".insertionSort");
+inSortbtn.addEventListener('click', async function(){
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    enableStopSortingBtn();
+    await insertion();
+    if(hasPressedStop==true){
+        disableSpeedSlider();
+    } else {
+        enableSortingBtn();
+        enableSizeSlider();
+    }
+    enableNewArrayBtn();
+    disableStopSortingBtn();
+});
